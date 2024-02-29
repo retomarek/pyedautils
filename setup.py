af45bf131c0e4887faa6cf5df7c27c96ba420861
Reto Marek <68020526+retomarek@users.noreply.github.com>
2024-02-29 22:02:18 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,3 @@
 import setuptools
-import some_build_toolkit
 
-def get_version():
-    version = some_build_toolkit.compute_version()
-    return version
-
-setuptools.setup(
-    version=get_version(),
-)
+setuptools.setup()
