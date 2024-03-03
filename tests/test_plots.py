1811a9cf065599a137ab212a181d33cd4095e5a3
Reto Marek <68020526+retomarek@users.noreply.github.com>
2024-03-03 17:15:07 +0100

    Update test_plots.py

--- tests/test_plots.py
+++ tests/test_plots.py
@@ -8,9 +8,11 @@ class TestPlots(unittest.TestCase):
     def test_plot_daily_profiles_overview(self):
         # Get the path to the current module
         current_dir = os.path.dirname(os.path.abspath(__file__))
-        
+        print("current_dir path:", current_dir)  # Debug print
         # Construct the path to the file relative to the package folder
-        file_path = "../pyedautils/data/ele_meter.csv"
+        #file_path = "../pyedautils/data/ele_meter.csv"
+        file_path = os.path.join(current_dir,'data', 'ele_meter.csv')
+        print("File path:", file_path)  # Debug print
 
         # Call the function under test
         df = load_data(file_path)
