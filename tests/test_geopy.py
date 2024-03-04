d5978499719a06bb16e0503627ecdcfbd6279d2f
Reto Marek <68020526+retomarek@users.noreply.github.com>
2024-03-04 13:59:12 +0100

    increased test coverage

--- tests/test_geopy.py
+++ tests/test_geopy.py
@@ -1,6 +1,9 @@
 # # -*- coding: utf-8 -*-
 
 import unittest
+import requests
+from requests.exceptions import RequestException
+from unittest.mock import patch
 from pyedautils.geopy import get_altitude_lat_long, get_lat_long_address, get_altitude_lv95, convert_wsg84_to_lv95, GeocodingError, get_coordindates_ch_plz, get_distance_between_two_points
 
 class TestGeopyhelpers(unittest.TestCase):
@@ -19,7 +22,14 @@ class TestGeopyhelpers(unittest.TestCase):
             get_altitude_lat_long(10, -200)
         with self.assertRaises(ValueError):
             get_altitude_lat_long(10, 200)
-
+    
+    @patch('requests.get')
+    def test_get_altitude_lat_long_no_internet(self, mock_get):
+        """Test get_altitude() without internet connection"""
+        mock_get.side_effect = RequestException("No internet connection")
+        with self.assertRaises(GeocodingError):
+            get_altitude_lat_long(47.01450, 8.30620)
+       
     def test_get_lat_long_address(self):
         """Test get_lat_long() with different addresses"""
         self.assertEqual(round(get_lat_long_address("Technikumstrasse 21, 6048 Horw")[0], 1), 47.0)
@@ -35,12 +45,25 @@ class TestGeopyhelpers(unittest.TestCase):
             convert_wsg84_to_lv95(57.01450, 8.30620)
         with self.assertRaises(GeocodingError):
             convert_wsg84_to_lv95(47.01450, 15.30620)
-    
+            
+    @patch('requests.get')
+    def test_convert_wsg84_to_lv95_no_internet(self, mock_get):
+        """Test convert_wsg84_to_lv95() without internet connection"""
+        mock_get.side_effect = RequestException("No internet connection")
+        with self.assertRaises(GeocodingError):
+            convert_wsg84_to_lv95(47.01331, 8.30612)
+
     def test_get_altitude_lv95(self):
         """Test get_altitude_ch_lv95() with different lat/long pairs"""
         self.assertIn(round(get_altitude_lv95([2665949.0,1207341.8]),0), range(438,445))
         self.assertIn(round(get_altitude_lv95(convert_wsg84_to_lv95(47.01450, 8.30620)),0), range(438,445))
-        
+
+    @patch('requests.get')
+    def test_get_altitude_lv95_no_internet(self, mock_get):
+        """Test get_altitude_ch_lv95() without internet connection"""
+        mock_get.side_effect = RequestException("No internet connection")
+        with self.assertRaises(GeocodingError):
+            get_altitude_lv95([2665949.0,1207341.8])
         
     def test_find_coordindates_ch_plz(self):
         self.assertAlmostEqual(get_coordindates_ch_plz(6048)[0], 47.01, delta=0.1)
