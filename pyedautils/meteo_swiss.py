8141cd56cb0ad2bc7214211d1b95a94bd3aa42d5
Reto Marek <68020526+retomarek@users.noreply.github.com>
2024-03-04 22:36:48 +0100

    Update meteo_swiss.py

--- pyedautils/meteo_swiss.py
+++ pyedautils/meteo_swiss.py
@@ -17,7 +17,7 @@ def get_current_station_data():
            
         return df
     except:
-        return pd.DataFrame()
+        raise ValueError("Error in getting data")
 
 def find_nearest_station(lat: float, long: float, altitude:float, sensor:str):
     """
@@ -32,60 +32,55 @@ def find_nearest_station(lat: float, long: float, altitude:float, sensor:str):
     Returns:
         str: Meteo Swiss station ID
     """
+    try:
+        switzerland_lat_min = 45.67
+        switzerland_lat_max = 47.92
+        switzerland_long_min = 5.7
+        switzerland_long_max = 10.7
+        
+        if(not((switzerland_lat_min <= lat <= switzerland_lat_max) and (switzerland_long_min <= long <= switzerland_long_max))):
+            raise ValueError("Coordinates not in range for Swiss coordinate system")
+        
+        all_station_data = get_current_station_data()
+        
+        lats=all_station_data["Breitengrad"].astype("float")
+        lons=all_station_data["Längengrad"].astype("float")
+        
+        distances=pd.DataFrame([get_distance_between_two_points((lat,long),(lats[i],lons[i])) for i in range(len(lats)) ])
+        distances.columns = ['value']
+        distances = distances.sort_values(by='value')
+        distances = distances.reset_index()
     
-    switzerland_lat_min = 45.67
-    switzerland_lat_max = 47.92
-    switzerland_long_min = 5.7
-    switzerland_long_max = 10.7
-    
-    if(not((switzerland_lat_min <= lat <= switzerland_lat_max) and (switzerland_long_min <= long <= switzerland_long_max))):
-        raise ValueError("Coordinates not in range for Swiss coordinate system")
-    
-    all_station_data = get_current_station_data()
-    
-    lats=all_station_data["Breitengrad"].astype("float")
-    lons=all_station_data["Längengrad"].astype("float")
-    
-    distances=pd.DataFrame([get_distance_between_two_points((lat,long),(lats[i],lons[i])) for i in range(len(lats)) ])
-    distances.columns = ['value']
-    distances = distances.sort_values(by='value')
-    distances = distances.reset_index()
-
-    i = 0
-    
-    while i < len(distances):
-        id = distances.loc[i,"index"]
-        sensors = all_station_data.loc[id,"Messungen"]        
-        found = False
-        stationID = all_station_data.loc[id,"Abk."]
-        stationIDString = all_station_data.loc[id,"Station"]
-        stationAltitude = float(all_station_data.loc[id,"Stationshöhe m ü. M."])
-        if((float(altitude) > (stationAltitude - 150.0)) and (float(altitude) < (stationAltitude + 150.0))):
-            if(sensor=="temp" and ("Temperatur" in sensors)):
-                print("Closest station for temp: " + str(stationIDString))
-                found = True
-                break
-            elif(sensor=="globrad" and ("Globalstrahlung" in sensors)):
-                print("Closest station for globalrad: " + str(stationIDString))
-                found = True
-                break
-            elif(sensor=="relhum" and ("Feuchte" in sensors)):
-                print("Closest station for relhum: " + str(stationIDString))
-                found = True
-                break
-            elif(sensor=="rain" and ("Niederschlag" in sensors)):
-                print("Closest station for rain: " + str(stationIDString))
-                found = True
-                break
+        i = 0
+        
+        while i < len(distances):
+            id = distances.loc[i,"index"]
+            sensors = all_station_data.loc[id,"Messungen"]        
+            stationID = all_station_data.loc[id,"Abk."]
+            stationIDString = all_station_data.loc[id,"Station"]
+            stationAltitude = float(all_station_data.loc[id,"Stationshöhe m ü. M."])
+            if((float(altitude) > (stationAltitude - 150.0)) and (float(altitude) < (stationAltitude + 150.0))):
+                if(sensor=="temp" and ("Temperatur" in sensors)):
+                    print("Closest station for temp: " + str(stationIDString))
+                    break
+                elif(sensor=="globrad" and ("Globalstrahlung" in sensors)):
+                    print("Closest station for globalrad: " + str(stationIDString))
+                    break
+                elif(sensor=="relhum" and ("Feuchte" in sensors)):
+                    print("Closest station for relhum: " + str(stationIDString))
+                    break
+                elif(sensor=="rain" and ("Niederschlag" in sensors)):
+                    print("Closest station for rain: " + str(stationIDString))
+                    break
+                else:
+                    i += 1
+                    continue
             else:
                 i += 1
                 continue
-        else:
-            i += 1
-            continue
     
-    if not found:
-        stationID = None
+        return stationID
     
-    return stationID
-
+    except:
+        raise ValueError("Error in getting data")
+    
