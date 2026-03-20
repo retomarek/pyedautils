19c47b9cf04d6341a149a381ab393e216fde54c7
Reto Marek <68020526+retomarek@users.noreply.github.com>
2026-03-20 13:23:25 +0100

    Update __init__.py

--- pyedautils/__init__.py
+++ pyedautils/__init__.py
@@ -2,5 +2,5 @@ from importlib.metadata import version, PackageNotFoundError
 
 try:
     __version__ = version("pyedautils")
-except PackageNotFoundError:
+except PackageNotFoundError:  # pragma: no cover
     __version__ = "unknown"
